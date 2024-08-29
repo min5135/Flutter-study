22bdc280cd9e19eb79227e1f45ce0b48c3dd3931
min5135 <hello.git.kimmin5566@naver.com>
2024-08-29 15:56:48 +0900

    fix: flutter 1일차

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,168 @@
-import 'package:flutter/material.dart';
+// 다트1
+//printInteger(int aNumber){
+//   print('The number is $aNumber.'); //콘솔에 출력
+// }
+//
+// int? couldReturnNullButDoesnt() => -3;
+//
+// //main() 함수에서 시작
+// main(){
+//   var number = 42; //동적타입 변수지정
+//   printInteger(number); //함수호출
+//
+//   int? couldBeNullButIsnt = 1;
+//   List<int?> listThatCouldHoldNulls = [2,null,4];
+//   List<int>? nullsList;
+//   int a = couldBeNullButIsnt;
+//   //int b = listThatCouldHoldNulls.first;
+//   int b = listThatCouldHoldNulls.first!;
+//   //int d = couldReturnNullButDoesnt().abs();
+//   int c = couldReturnNullButDoesnt()!.abs();
+//
+//   print('a is $a');
+//   print('b is $b');
+//   print('c is $c');
+// }
 
-void main() {
-  runApp(const MyApp());
+/*void main() async{
+  checkVersion();
+  await getVersionName().then((value) =>{
+    print(value)
+  });
+  print('end process');
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<String> getVersionName()async{
+  var versionName = await lookUpVersionName();
+  return versionName;
+}
 
-  // This widget is the root of your application.
+String lookUpVersionName() {
+  return 'Android Q';
+}
+
+Future checkVersion() async{
+  var version = await lookUpVersion();
+  print(version);
+}
+
+int lookUpVersion(){
+  return 12;
+}
+
+ */
+/*
+void printOne(){
+  print('one');
+}
+Future<void> printTwo() async {
+  await Future.delayed(Duration(seconds: 1),(){
+    print('Future!!');
+  });
+  print('Two');
+}
+void printThree(){
+  print('Three');
+}
+
+void main(){
+  printOne();
+  printTwo();
+  printThree();
+}
+
+ */
+/* 다트 2-3
+import 'dart:convert';
+// ''' <<  그대로 인식을 할때 쓰임 그냥 받아드린다
+//ex) '''  asjdasjdlkasjldksajdklajskdlj ''';
+void main(){
+  var jsonString = '''
+  [
+    {"score" : 40},
+    {"score" : 80}
+  ]
+  ''';
+  var scores = jsonDecode(jsonString);
+  print(scores is List); // score 가 list야? []이기때문에 리스트가 된다
+  var firstScore = scores[0];
+  print(firstScore is Map); // fisrt score 가 map이야?  map이다.
+  print(firstScore['score'] == 40); //score = 40 이야? true
+
+
+  //List<MAP>
+  var scores1 = [
+    {'score' : 40},
+    {'score' : 80},
+    {'score' : 100 , 'overtime':true, 'special_guest':null}
+  ];
+  var jsonText = jsonEncode(scores1); //List -> json
+
+  print(jsonText ==
+    '[{"score":40},{"score":80},'
+      '{"score":100, "overtime":true,'
+      '"special_guest":null}]'); //true출력
+
+}
+
+
+ */
+
+
+/* dart stream
+import 'dart:async';
+
+Future<int> sumStream(Stream<int> stream) async{
+  var sum=0;
+  await for (var value in stream){
+    print("sumStream : $value" );
+    sum += value;
+  }
+  return sum;
+}
+
+Stream<int> countStream(int to) async*{
+  for(int i = 1; i<= to; i++){
+    print('countStream : $i');
+    yield i;
+  }
+}
+
+main() async {
+  var stream = countStream(10);
+  var sum = await sumStream(stream);
+  print(sum); //55
+
+  var stream1 = Stream.fromIterable([1,2,3,4,5]);
+  //가장 앞의 데이터 결과:1
+  stream1.first.then((value) => print('first: $value'));
+  //가장 마지막 데이터 결과:5
+  stream1.last.then((value) => print('last: $value'));
+  //현재 스트림이 비어있는지 확인
+  stream1.isEmpty.then((value) => print('isEmpty: $value'));
+  //전체 길이 : 5
+  stream1.length.then((value) => print('length: $value'));
+}
+
+ */
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Center(child: Text('hello\nFlutter',textAlign: TextAlign.center)),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
